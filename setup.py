fe7fff6f1b912b5c0abad8fa89f0313dec566d63
Juan Moises de la Serna <jmsernatuya@gmail.com>
2026-03-25 08:59:50 +0100

    Add setup.py: example notebook, Python package, HF card, Kaggle metadata

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+setup(
+    name="hiv-evolution-latin-america-2000-2024",
+    version="1.0.0",
+    description="Evolución del VIH en Latinoamérica: Datos y Cifras 2000-2024. DOI: 10.17632/nymmw3955c.1",
+    author="de la Serna, Juan Moisés",
+    url="https://github.com/juanmoisesd/hiv-evolution-latin-america-2000-2024",
+    packages=find_packages(),
+    install_requires=["pandas>=1.3.0","requests>=2.26.0"],
+    python_requires=">=3.7",
+    classifiers=["Programming Language :: Python :: 3","License :: OSI Approved :: MIT License","Topic :: Scientific/Engineering"],
+    keywords="zenodo, open-data",
+)
